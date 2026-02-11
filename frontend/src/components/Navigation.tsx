bed287154d9a818a124d7f4849fe2a92d40a0a06
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2026-02-11 17:32:17 -0500

    guest navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -22,12 +22,10 @@ const Navigation: React.FC = () => {
   return (
     <div className="nav-container">
       <div className="logo left">ENCORE</div>
-      {isLoggedIn && (
         <div className="pages middle">
           <div id="journal" onClick={handleJournal}>journal entries</div>
           <div id="calendar" onClick={handleCalendar}>dashboard</div>
         </div>
-      )}
       <div className="pages right">
       <div id="account" onClick={handleAccount} style={{ cursor: isLoggedIn ? "pointer" : "default" , fontWeight: "bold" }}>{isLoggedIn ? "my account" : "sign up / login"}</div>
 
